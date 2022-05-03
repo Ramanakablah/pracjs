a90669f93f6f097e3113ac2f26e07ec4c196e97f
Raman <alexesramon0909@gmail.com>
2022-05-03 09:15:21 +0530

    added port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express(); 
-const Port = 7000 || Process.env;
+const Port = 7000 || Process.env.PORT;
 
 app.get('/',(req,res)=>{
     res.send('hello there')
