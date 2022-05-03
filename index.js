c613f3aaee193fbfb2eacb6b05a3eccb83fe0b62
Raman <alexesramon0909@gmail.com>
2022-05-03 10:23:56 +0530

    procfile port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express(); 
-const PORT = 7000 || Process.env.PORT;
+const PORT = 7000 || process.env.PORT;
 
 app.get('/',(req,res)=>{
     res.send('hello there')
