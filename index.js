1d7b3b057896997914b074930b842c6b8ca7d453
Raman <alexesramon0909@gmail.com>
2022-05-03 09:17:31 +0530

    make it better

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express(); 
-const Port = 7000 || Process.env.PORT;
+const Port = 7000 || Process.env.port;
 
 app.get('/',(req,res)=>{
     res.send('hello there')
