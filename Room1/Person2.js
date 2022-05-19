8221a55cc239d0b5ee87b02aa7abf14de42e663c
Raman <alexesramon0909@gmail.com>
2022-05-19 08:28:27 +0530

    Middleware added

--- Room1/Person2.js
+++ Room1/Person2.js
@@ -1,5 +1,7 @@
 const express  = require('express')
+const res = require('express/lib/response')
 const app = express()
+const fs = require("fs")
 const router = express.Router()
 
 router.get('/greet',(req,res)=>{
@@ -20,6 +22,57 @@ router.post('/handshake',(req,res)=>{
         
     }
 
+})
+
+router.get('/whttosay',async (req,res)=>{
+    try {
+        let Life = {}     
+        let object={
+            name:"",
+            age:0,
+            exp:10
+        }
+
+            for(let index = 0; index < 100; index++){
+             let obj = new object;
+             obj.age=index ;
+             obj.exp = index+0.2;
+             Life[index]=object
+         }
+
+         fs.writeFileSync('../Exp_dat/Data.json',Life)
+
+    } catch (error) {
+        
+    }
+})
+
+router.post('/listening' , (req,res)=>{
+   try {
+       let pile = req.body.Pile;
+
+       
+   } catch (error) {
+       console.log(error);
+       res.end(error)
+   }
+})
+
+router.post("/file" ,(req,res)=>{
+    try {
+        console.log("Waiting for file");
+        let file = req.body.upfile;
+        console.log("file recieved???");
+        if(file){
+            console.log(file);
+            res.status(200).send("Ok");
+        }
+        else{
+            console.log("File not uploaded");
+        }
+    } catch (error) {
+      res.status(400).send("Servert crashed")
+    }
 })
 
 
--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express(); 
 const Port = process.env.PORT || 5000;
 
+app.use((req,res,next)=>{
+    res.append('Access-Control-Allow-Origin',['*']);
+    res.append('Access-Control-Allow-Method','GET,POST,PUT,DELETE');
+    res.append('Access-Control-Allow-Heades','Content-Type')
+    next()
+})
+
 app.get('/',(req,res)=>{
     res.send('hello there')
 })
